ddf79e18ec48737a7043fc7f09a25c35a62dd329
Prabin Thapa Magar <prabinmaqar@gmail.com>
2026-08-12 12:21:18 +0545

    Handling multiple descriptors using select()

---
+++ Lab_4/client.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+
+    int sock;
+    struct sockaddr_in serv_addr;
+    char buffer[1024];
+    char message[] = "Hello Select Server";
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    connect(sock,
+            (struct sockaddr *)&serv_addr,
+            sizeof(serv_addr));
+
+    send(sock, message, strlen(message), 0);
+
+    read(sock, buffer, sizeof(buffer));
+
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+
+    return 0;
+}
