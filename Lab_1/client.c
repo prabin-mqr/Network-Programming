1ea479a86ddf535f3cae95085430a75b7d5b4dd2
Prabin Thapa Magar <prabinmaqar@gmail.com>
2026-08-12 12:17:48 +0545

    Basic TCP client-server application

---
+++ Lab_1/client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char *message = "Hello from Client";
+
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IP address
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address\n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection failed\n");
+        return -1;
+    }
+
+    // Send message
+    send(sock, message, strlen(message), 0);
+    printf("Message sent to server.\n");
+
+    // Receive reply
+    read(sock, buffer, sizeof(buffer));
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+
+    return 0;
+}
